2ab66f040ce68618e4a6cdd422391a041a698eae
Kevin-Detournay <kevindetournay@hotmail.fr>
2021-03-29 11:23:06 +0200

    replacing cors at the good place

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
+const router = require('./app/router.js')
+const sanitizer = require('./app/services/bodySanitizer');
+app.use(express.urlencoded({ extended: true }));
+
+
+
 // CORS
 const options = {
     origin: 'https://switchecommerce.netlify.app/'}
 
 app.use(cors(options));
 
-const sanitizer = require('./app/services/bodySanitizer');
-app.use(express.urlencoded({ extended: true }));
-const router = require('./app/router.js')
-
-
-
-
 
 // app.use(cors('*'));
+
 
 // parser json which gets the payload and transforms it into a js object available under request.body
 app.use(express.json());
